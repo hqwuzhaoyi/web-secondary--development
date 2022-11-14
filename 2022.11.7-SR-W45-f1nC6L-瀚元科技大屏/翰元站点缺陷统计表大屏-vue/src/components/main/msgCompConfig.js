693461f83fa8f5601f10ec1b7b54fd868de195d1
YoungRuntu <97159451+YoungRuntu@users.noreply.github.com>
2022-11-14 16:50:21 +0800

    2022 q4 (#362)
    * feat : 需求编号：SR-W44-m2ZWaD;需求：瀚元科技 应用顶栏二开。状态：完成;
    需求编号：SR-W44-aBaLhe;需求：杭州衡成  停车场监控。状态：完成;
    
    * feat:需求编号:SR-W45-f1nC6L;是否完成：是；

---
+++ 2022.11.7-SR-W45-f1nC6L-瀚元科技大屏/翰元站点缺陷统计表大屏-vue/src/components/main/msgCompConfig.js
@@ -0,0 +1,31 @@
+// 组件可派发事件
+export const events = [];
+
+// 组件可接收事件
+export const actions = [
+  {
+    key: "changeColor",
+    name: "改变颜色",
+    params: [
+      {
+        key: "color",
+        name: "颜色",
+        dataType: "string"
+      }
+    ],
+    hasReturn: false
+    // // hasReturn为false则不用写returns选项
+    // returns: [
+    //   {
+    //     key: "value",
+    //     name: "值",
+    //     dataType: "string"
+    //   }
+    // ]
+  }
+];
+
+export default {
+  actions,
+  events
+};
---
+++ 2022.11.7-SR-W45-f1nC6L-瀚元科技大屏/翰元设备缺陷统计表大屏-vue/src/components/index.js
@@ -0,0 +1,11 @@
+import DataConfiguration from "./configuration/data/App.vue";
+import StyleConfiguration from "./configuration/style/App.vue";
+import OtherConfiguration from "./configuration/other/App.vue";
+import Main from "./main/App.vue";
+
+export {
+  DataConfiguration,
+  StyleConfiguration,
+  OtherConfiguration,
+  Main
+};
